70d89c1f3398c30e47c74d5ee74048af37f93920
aidendoubleday <1301279068@s.tusd1.org>
2026-03-05 16:22:13 +0000

    progress

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Script started");
+
+function changeStyle() {
+    console.log("click");
+
+    let p = document.getElementById("text");
+    p.style.margin = "50px";
+    p.style.color = "grey";
+    p.style.fontSize = "40px";
+    p.style.textAlign = "center";
+}
